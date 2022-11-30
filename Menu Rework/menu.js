bc27a4c9ec6c43647fb0b507429e12da24481c3f
Petar Bursakj <petar@thrasker.com>
2022-11-30 19:00:06 +0100

    alpha alpha 1.1 and rework

---
+++ Menu Rework/menu.js
@@ -0,0 +1,8 @@
+let btnCoffee = document.getElementById("btnCoffee");
+let divCoffee = document.getElementById("divCoffee");
+
+btnCoffee.addEventListener ("click" , () => {
+
+    divCoffee.classList.toggle("show");
+
+})
--- menu.js
+++ menu.js
@@ -10,116 +10,121 @@ fetch("https://finalbossslayer.github.io/Menu/Data/items.json")
     // Making every row
     for (const s in inp) {
 
-        // Generating all the html elements we need
-        let leadCon = document.createElement("div");
-        let secendaryCon = document.createElement("div");
-        let btnPM = document.createElement("button");
-        let textBtn = document.createElement("p");
-        let imgPlus = document.createElement("img");
-        let imgMinus = document.createElement("img");
-    
-        // All the main containers are appended here
-        main.append(leadCon);
-        leadCon.append(btnPM);
-        leadCon.append(secendaryCon);
-    
-        // The Dropdown of every row
-        btnPM.addEventListener("click" , () => {
-            secendaryCon.classList.toggle("show");
-            imgPlus.classList.toggle("hide");
-            imgMinus.classList.toggle("show");
-        })
-    
-        // Adding classes
-        leadCon.classList.add("dropdown");
-        btnPM.classList.add("dropbtn");
-        textBtn.classList.add("textBtnClass");
-    
-        // The Secondary container
-        secendaryCon.classList.add("dropdown-content");
-        let table = document.createElement("table");
-        secendaryCon.append(table);
-    
-        // Adding a name for the row
-        textBtn.append(`${s}`);
-        btnPM.append(textBtn);
-    
+            // Generating all the html elements we need
+            let leadCon = document.createElement("div");
+            let secendaryCon = document.createElement("div");
+            let btnPM = document.createElement("button");
+            let textBtn = document.createElement("p");
+            let imgPlus = document.createElement("img");
+            let imgMinus = document.createElement("img");
+
+        if(s != "bgi"){
+
+            // All the main containers are appended here
+            main.append(leadCon);
+            leadCon.append(btnPM);
+            leadCon.append(secendaryCon);
         
-        let arr2 = []
-
-        // Elements for every row
-        for (const d in inp[s]) {
-
-            // Generating elements
-            let tr = document.createElement("tr");
-            let td1 = document.createElement("td");
-            let td2 = document.createElement("td");
-            let td3 = document.createElement("td");
-            let imgTable = document.createElement("img");
-            let textTable = document.createElement("p");
-            let numTable = document.createElement("p");
-                
-            // Image for the menu item
-            imgTable.setAttribute("src" , `images/${inp[s][d].image}`)
-
-            console.log(inp[s][d].image)
+            // The Dropdown of every row
+            btnPM.addEventListener("click" , () => {
+                secendaryCon.classList.toggle("show");
+                imgPlus.classList.toggle("hide");
+                imgMinus.classList.toggle("show");
+            })
+        
+            // Adding classes
+            leadCon.classList.add("dropdown");
+            btnPM.classList.add("dropbtn");
+            textBtn.classList.add("textBtnClass");
+        
+            // The Secondary container
+            secendaryCon.classList.add("dropdown-content");
+            let table = document.createElement("table");
+            secendaryCon.append(table);
+        
+            // Adding a name for the row
+            textBtn.append(`${s}`);
+            btnPM.append(textBtn);
+        
             
-            imgTable.classList.add("imgTable")
-            table.append(tr)
-            tr.append(td1)
-            td1.append(imgTable)
+            let arr2 = []
 
-            td1.classList.add("tdBot")
-            td1.classList.add("td1")
-                
-            // Name of the menu item
-            textTable.append(`${d}`)
-                
-            textTable.classList.add("textTable")
-            tr.append(td2)
-            td2.append(textTable)
+            // Elements for every row
+            for (const d in inp[s]) {
 
-            td2.classList.add("tdLeft")
-            td2.classList.add("tdBot")
-            td2.classList.add("td2")
-                
-            // Price of the menu item
-            numTable.append(inp[s][d].price)
+                // Generating elements
+                let tr = document.createElement("tr");
+                let td1 = document.createElement("td");
+                let td2 = document.createElement("td");
+                let td3 = document.createElement("td");
+                let imgTable = document.createElement("img");
+                let textTable = document.createElement("p");
+                let numTable = document.createElement("p");
                     
-            
-            numTable.classList.add("numTable")
-            tr.append(td3)
-            td3.append(numTable)
-
-            td3.classList.add("tdLeft")
-            td3.classList.add("tdBot")
-            td3.classList.add("td3")
-
-            // To make the last element in the row not have a line under neeth (aka select the last element)
-            let arr1 = Object.keys(inp[s]).length
-            arr2.push("1")
+                // Image for the menu item
+                imgTable.setAttribute("src" , `images/${inp[s][d].image}`)
                 
-            if (arr2.length == arr1) {
-                td1.classList.remove("tdBot")
-                td2.classList.remove("tdBot")
-                td3.classList.remove("tdBot")
-            }
+                imgTable.classList.add("imgTable")
+                table.append(tr)
+                tr.append(td1)
+                td1.append(imgTable)
 
+                td1.classList.add("tdBot")
+                td1.classList.add("td1")
+                    
+                // Name of the menu item
+                textTable.append(`${d}`)
+                    
+                textTable.classList.add("textTable")
+                tr.append(td2)
+                td2.append(textTable)
+
+                td2.classList.add("tdLeft")
+                td2.classList.add("tdBot")
+                td2.classList.add("td2")
+                    
+                // Price of the menu item
+                numTable.append(inp[s][d].price)
+                        
+                
+                numTable.classList.add("numTable")
+                tr.append(td3)
+                td3.append(numTable)
+
+                td3.classList.add("tdLeft")
+                td3.classList.add("tdBot")
+                td3.classList.add("td3")
+
+                // To make the last element in the row not have a line under neeth (aka select the last element)
+                let arr1 = Object.keys(inp[s]).length
+                arr2.push("1")
+                    
+                if (arr2.length == arr1) {
+                    td1.classList.remove("tdBot")
+                    td2.classList.remove("tdBot")
+                    td3.classList.remove("tdBot")
+                }
+
+            }
         }
 
-        // For a backrownd image on the row
-        btnPM.style.backgroundImage = `url(${inp[s][2]})`;
-            
-            
+            // For a backrownd image on the row
+            for (const d in inp[s]) {
+                if(s == "bgi"){
+                    btnPM.style.backgroundImage = `url(${inp[s][d]})`;
+                }
+            }
+                
+                
 
-        // The plus and minus icons
-        btnPM.append(imgPlus);
-        imgPlus.setAttribute("src", "plus.png")
-        imgPlus.classList.add("imgPlusClass")
-        
-        btnPM.append(imgMinus);
-        imgMinus.setAttribute("src", "minus.png")
-        imgMinus.classList.add("imgMinusClass")
+            // The plus and minus icons
+            btnPM.append(imgPlus);
+            imgPlus.setAttribute("src", "plus.png")
+            imgPlus.classList.add("imgPlusClass")
+            
+            btnPM.append(imgMinus);
+            imgMinus.setAttribute("src", "minus.png")
+            imgMinus.classList.add("imgMinusClass")
 
     }
 });
