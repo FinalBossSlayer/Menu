e2d5932deb6f503febc4757fa9e3aa03301ede58
Petar Bursakj <petar@thrasker.com>
2023-02-03 20:53:19 +0100

    menu beta 1.0

--- Menu Rework/menu.js
+++
@@ -1,8 +0,0 @@
-let btnCoffee = document.getElementById("btnCoffee");
-let divCoffee = document.getElementById("divCoffee");
-
-btnCoffee.addEventListener ("click" , () => {
-
-    divCoffee.classList.toggle("show");
-
-})
